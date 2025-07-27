a0dac6812cbbe7de5367d4f2789ff795ad5adf2c
ba2hankaya <baa2han.kay@gmail.com>
2025-07-27 16:25:23 +0300

    implemented curses ui, using a non blocking input system to stay on a single thread. new class for outputting received messages. new class for getting input from user.

--- frontend.py
+++ frontend.py
@@ -1,10 +1,76 @@
 import asyncio
 import sys
 import os
+import curses
+
+#RECEIVE_WINDOW_PARAMS
+r_begin_x = 1
+r_begin_y = 1
+r_height = 30
+r_width = 200
+
+#INPUT_WINDOW_PARAMS
+i_begin_x = 1
+i_begin_y = 32
+i_height = 3
+i_width = 200
+
+
+class NonBlockingInput:
+    def __init__(self, inputwin, backendwriter):
+        self.inputwin = inputwin 
+        self.backendwriter = backendwriter
+        self.s = ""
+    async def build_input(self):
+        self.redraw()
+        key = self.inputwin.getch()
+        if key == curses.ERR:
+            return
+        elif key in (curses.KEY_ENTER, 10, 13):
+            await send_msg(self.backendwriter, self.s)
+            self.s = ""
+        elif key in (curses.KEY_BACKSPACE, 127):
+            if(len(self.s) > 0):
+                self.s = self.s[:-1]
+        elif 32 <= key <= 126:
+            self.s += str(chr(key))
+
+    def redraw(self):
+        self.inputwin.clear()
+        self.inputwin.box()
+        self.inputwin.addstr(1, 1, self.s)
+        self.inputwin.refresh()
+        
+
+class OutputReceived:
+    def __init__(self, receivewin):
+        self.receivewin = receivewin
+        self.messages = []
+
+    def add_message(self, message):
+        if message == "":
+            return
+        self.messages.append(message)
+        if len(self.messages) == r_height - 1:
+            self.messages.pop(0)
+        self.redraw()
+
+    def redraw(self):
+        self.receivewin.clear()
+        self.receivewin.box()
+        self.output()
+        self.receivewin.refresh()
+
+    def output(self):
+        curY = 1
+        for message in self.messages:
+            self.receivewin.addstr(curY, 1, message)
+            curY +=1 
+    
+
 
 async def main():
     server = await asyncio.start_server(handle_comms, '127.0.0.1' , 33445)
-
     #script_path = os.path.join(os.path.dirname(__file__), 'client.py')
     #print("Starting backend process...")
     #backend_proc = await asyncio.create_subprocess_exec(
@@ -17,39 +83,65 @@ async def main():
         await server.serve_forever()
 
 async def handle_comms(reader, writer):
-    serverip = input("Enter server ip: ")
-    port = input("Enter server port: ")
-
-    writer.write((serverip + '\n').encode())
-    await writer.drain()
-
-    writer.write((port + '\n').encode())
-    await writer.drain()
-
-    await asyncio.gather(
-            receive_loop(reader),
-            send_loop(writer)
-    )
-
-async def receive_loop(backendreader):
     try:
+        serverip = input("Enter server ip: ")
+        port = input("Enter server port: ")
+
+        writer.write((serverip + '\n').encode())
+        await writer.drain()
+
+        writer.write((port + '\n').encode())
+        await writer.drain()
+
+        stdscr = curses.initscr()
+
+        receivewin = curses.newwin(r_height, r_width, r_begin_y, r_begin_x)
+        inputwin = curses.newwin(i_height, i_width, i_begin_y, i_begin_x)
+        inputwin.box()
+        inputwin.nodelay(True)
+        receivewin.box()
+        await asyncio.gather(
+                receive_loop(reader, receivewin),
+                send_loop(inputwin, writer)
+        )
+    except Exception as e:
+        print(e)
+    finally:
+        curses.endwin()
+
+async def receive_loop(backendreader, receivewin):
+    try:
+        outrecv = OutputReceived(receivewin)
         while True:
             data = await backendreader.readline()
             if not data:
                 print("backend disc")
                 sys.exit()
             message = data.decode().strip()
-            print(f"received from backend: {message}")
+            outrecv.add_message(message)
+            #print(f"received from backend: {message}")
     except (ConnectionError, asyncio.IncompleteReadError, asyncio.CancelledError):
         print("connection to backend was lost while receiveing")
 
-async def send_loop(backendwriter):
+async def send_msg(backendwriter, msg):
     try:
+        backendwriter.write((msg + '\n').encode())
+        await backendwriter.drain()
+    except Exception as e:
+        print(e)
+
+
+async def send_loop(inputwin, backendwriter):
+    try:
+        nbi = NonBlockingInput(inputwin, backendwriter)
         while True:
-            userinput = await asyncio.to_thread(input, "> ")
-            backendwriter.write((userinput + '\n').encode())
-            await backendwriter.drain()
+            await nbi.build_input()
+            await asyncio.sleep(0.05)
+            #userinput = await asyncio.to_thread(input, "> ")
+            #backendwriter.write((userinput + '\n').encode())
+            #await backendwriter.drain()
     except(ConnectionError, asyncio.CancelledError):
         print("connection to backend was lost while sending")
+
 
 asyncio.run(main())
